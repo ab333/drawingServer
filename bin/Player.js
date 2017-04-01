95fe2c2bf959ae243431ba4da5268e301d7add06
ab333 <ab333@users.noreply.github.com>
2017-04-01 13:56:45 +0300

    .

---
+++ bin/Player.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Abos on 01/04/2017.
+ */
+function Player (username, socketId) {
+    this.username = username;
+    this.socketId = socketId;
+}
+
+Player.prototype.getInfo = function() {
+    return this.username + ' ' + this.socketId + ' apple';
+};
+
+function Room (Id) {
+    this.players = [];
+    this.Id = Id;
+    this.channelId = "";
+    this.status = "";
+    this.word1 = "";
+    this.word2 = "";
+    this.spy = "";
+    this.currentTurn = "";
+    this.numOfPlayers = "";
+    this.turnOrder = "";
+}
+
+Room.prototype.getInfo = function() {
+    return this.Id + ' ' + this.Id + ' apple';
+};
+
